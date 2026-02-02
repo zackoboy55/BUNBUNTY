c0e872c9dbe8cf06b9e0e5c26e1633537cd1026d
zackoboy55 <130990353+zackoboy55@users.noreply.github.com>
2026-02-02 16:54:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ noBtn.addEventListener("mouseover", () => {
 // YES is clicked
 
 yesBtn.addEventListener("click", () => {
-    title.textContent = "Yippeeee!";
+    title.textContent = "Mwaahhhh!";
 
     catImg.src = "cat_dance.gif";
 
